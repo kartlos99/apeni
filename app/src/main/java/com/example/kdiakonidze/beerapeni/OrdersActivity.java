8bb3b454684ef4d7d83351403420e1b439cf506c
kartlos99 <kartlos99@yahoo.com>
2017-10-31 18:42:29 +0400

    შეკვეტა მიტანის ცხრილი მოაქვს არჩეული დროის მიხედვით, მოტრიალებაზე იგივე ინფოს ტვირთავს თავიდან

--- app/src/main/java/com/example/kdiakonidze/beerapeni/OrdersActivity.java
+++ app/src/main/java/com/example/kdiakonidze/beerapeni/OrdersActivity.java
@@ -2,9 +2,11 @@ package com.example.kdiakonidze.beerapeni;
 
 import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
+import android.icu.text.SimpleDateFormat;
 import android.icu.util.Calendar;
 import android.icu.util.TimeZone;
 import android.os.Build;
+import android.os.PersistableBundle;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,10 +17,13 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpClientStack;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.kdiakonidze.beerapeni.adapters.ShekvetebiAdapter;
 import com.example.kdiakonidze.beerapeni.models.Shekvetebi;
@@ -31,79 +36,88 @@ import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Date;
 
+@RequiresApi(api = Build.VERSION_CODES.N)
 public class OrdersActivity extends AppCompatActivity {
 
+    String archeuli_dge;
     Button btn_setDate;
     TextView t_Tarigi;
     ListView listView_shekvetebi;
     ProgressDialog progressDialog;
     ArrayList<Shekvetebi> shekvetebiArrayList;
     ShekvetebiAdapter shekvetebiAdapter;
+    Calendar calendar;
+    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
 
     DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
         @Override
-        public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
-            i1++;
-            btn_setDate.setText(i+"-"+i1+"-" + i2);
+        public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+            calendar.set(year,month,day);
+            archeuli_dge = dateFormat.format(calendar.getTime());
+            btn_setDate.setText(archeuli_dge);
+            shekvetebis_chamotvirtva(archeuli_dge);
         }
     };
 
+    @Override
+    public void onSaveInstanceState(Bundle outState){
 
+        outState.putString("tarigi", archeuli_dge);
+
+        super.onSaveInstanceState(outState);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_orders);
 
+
         listView_shekvetebi = (ListView) findViewById(R.id.list_shekvetebi);
         t_Tarigi = (TextView) findViewById(R.id.t_tarigi);
         btn_setDate = (Button) findViewById(R.id.btn_setdate);
 
-        t_Tarigi.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.N)
-            @Override
-            public void onClick(View view) {
-                Date curtime = Calendar.getInstance().getTime();
-                t_Tarigi.setText(curtime.toString());
-            }
-        });
+        calendar = Calendar.getInstance();
+        calendar.add(Calendar.HOUR,4);
 
+        if(savedInstanceState!=null){
+            archeuli_dge = savedInstanceState.getString("tarigi");
+            Toast.makeText(this,archeuli_dge, Toast.LENGTH_SHORT).show();
+        }else {
+            archeuli_dge = dateFormat.format(calendar.getTime());
+        }
+        btn_setDate.setText(archeuli_dge);
+        shekvetebis_chamotvirtva(archeuli_dge);
 
         btn_setDate.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void onClick(View view) {
-                Calendar c = Calendar.getInstance();
-                c.add(Calendar.HOUR,4);
-//                Date curtime = Calendar.getInstance().getTime();
-                t_Tarigi.setText(c.getTime().toString());
 
-
-                btn_setDate.setText(c.get(Calendar.YEAR)+"/"+ c.get(Calendar.MONTH)+"/"+ c.get(Calendar.DAY_OF_MONTH)+"/"+ c.get(Calendar.HOUR_OF_DAY)+"/"+ c.get(Calendar.MINUTE));
-
-                DatePickerDialog datePickerDialog = new DatePickerDialog(OrdersActivity.this, dateSetListener, c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH) );
+                DatePickerDialog datePickerDialog = new DatePickerDialog(OrdersActivity.this, dateSetListener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
                 datePickerDialog.setCancelable(false);
                 datePickerDialog.show();
             }
         });
 
-        shekvetebis_chamotvirtva();
+
 
 
     }
 
-    private void shekvetebis_chamotvirtva() {
+    private void shekvetebis_chamotvirtva(String currentDay) {
         progressDialog = ProgressDialog.show(this,"იტვირთება!", "loading!");
         shekvetebiArrayList = new ArrayList<Shekvetebi>();
-        String url = Constantebi.URL_GET_ORDERLIST;
+        String url = Constantebi.URL_GET_ORDERLIST +"?tarigi="+currentDay;
 
         RequestQueue queue = Volley.newRequestQueue(this);
 
-        JsonArrayRequest requestObieqtebi = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
+        JsonArrayRequest requestObieqtebi = new JsonArrayRequest( url, new Response.Listener<JSONArray>() {
             @Override
             public void onResponse(JSONArray response) {
                 // aq modis obieqtebis chamonatvali
-
 
                 if(response.length() > 0){
                     shekvetebiArrayList.clear();
@@ -124,8 +138,6 @@ public class OrdersActivity extends AppCompatActivity {
                             excep.printStackTrace();
                         }
                     }
-
-
                 }
 
                 shekvetebiAdapter = new ShekvetebiAdapter(getApplicationContext(),shekvetebiArrayList);
