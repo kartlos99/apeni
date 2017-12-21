699ab48c790fe1a2f6b7593432dbea8b74d69c7f
kartlos99 <kartlos99@yahoo.com>
2017-12-21 16:53:08 +0400

    რეალიზაცია დისტრიბუტორების მიხედვით

--- app/src/main/java/com/example/kdiakonidze/beerapeni/DaySaleActivity.java
+++ app/src/main/java/com/example/kdiakonidze/beerapeni/DaySaleActivity.java
@@ -8,10 +8,16 @@ import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -34,34 +40,39 @@ import java.util.ArrayList;
 public class DaySaleActivity extends AppCompatActivity {
 
     private ArrayList<SaleInfo> salesDay;
-    Calendar calendar;
-    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-    String archeuli_dge;
-    Button btn_setDate;
-    TextView tTarigi, t_k30count, t_k50count, t_laricount;
-    ListView saleslistView;
-    DaySalesAdapter salesAdapter;
-    ProgressDialog progressDialog;
-
+    private Calendar calendar;
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    private String archeuli_dge;
+    private Button btn_setDate;
+    private TextView tTarigi, t_k30count, t_k50count, t_laricount;
+//    private ListView saleslistView;
+    private DaySalesAdapter salesAdapter;
+    private ProgressDialog progressDialog;
+    private Spinner sp_distr;
+    private String distr_id = "0";
+    private RequestQueue queue;
+    private Boolean requestInProgres = false, requestNeeded = true;
+    NonScrollListView nonScrolSaleslistView;
 
     DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
         @RequiresApi(api = Build.VERSION_CODES.N)
         @Override
         public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-            calendar.set(year,month,day);
+            calendar.set(year, month, day);
             archeuli_dge = dateFormat.format(calendar.getTime());
             btn_setDate.setText(archeuli_dge);
-            getsales(archeuli_dge);
-            tTarigi.setText("დღიური რეალიზაცია\nთარიღი "+archeuli_dge);
+            getsales(archeuli_dge, distr_id);
+            tTarigi.setText("დღიური რეალიზაცია\nთარიღი " + archeuli_dge);
         }
     };
 
     @Override
-    public void onSaveInstanceState(Bundle outState){
+    public void onSaveInstanceState(Bundle outState) {
 
         outState.putString("tarigi", archeuli_dge);
-        outState.putSerializable("sales_list",salesDay);
-
+        outState.putSerializable("sales_list", salesDay);
+        outState.putBoolean("progress", requestInProgres);
+        outState.putInt("distrId", sp_distr.getSelectedItemPosition());
         super.onSaveInstanceState(outState);
     }
 
@@ -69,32 +80,72 @@ public class DaySaleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_day_sale);
+
+        queue = Volley.newRequestQueue(this);
+
         btn_setDate = (Button) findViewById(R.id.btn_tarigi);
         tTarigi = (TextView) findViewById(R.id.t_tarigi);
         t_k30count = (TextView) findViewById(R.id.t_p3_k30_count);
         t_k50count = (TextView) findViewById(R.id.t_p3_k50_count);
-        t_laricount= (TextView) findViewById(R.id.t_p3_lari_count);
-        saleslistView = (ListView) findViewById(R.id.sales_list);
+        t_laricount = (TextView) findViewById(R.id.t_p3_lari_count);
+//        saleslistView = (ListView) findViewById(R.id.sales_list);
+        sp_distr = (Spinner) findViewById(R.id.sp_distributori);
+        nonScrolSaleslistView = (NonScrollListView) findViewById(R.id.sales_list1);
 
         salesDay = new ArrayList<>();
 
         calendar = Calendar.getInstance();
-        calendar.add(Calendar.HOUR,4);
+        calendar.add(Calendar.HOUR, 4);
 
-        if(savedInstanceState!=null){
+        if (savedInstanceState != null) { // recreate moxda
             archeuli_dge = savedInstanceState.getString("tarigi");
+            requestNeeded = false;
+//            sp_distr.setSelection(savedInstanceState.getInt("distrId"));
+            requestInProgres = savedInstanceState.getBoolean("progress");
+            if (requestInProgres) {
+                progressDialog = ProgressDialog.show(this, "იტვირთება!", "დაელოდეთ!");
+            }
             salesDay.clear();
             salesDay = (ArrayList<SaleInfo>) savedInstanceState.getSerializable("sales_list");
             salesAdapter = new DaySalesAdapter(getApplicationContext(), salesDay);
-            saleslistView.setAdapter(salesAdapter);
+            nonScrolSaleslistView.setAdapter(salesAdapter);
             showAtherInfo(salesDay);
-        }else {
+        } else {
             archeuli_dge = dateFormat.format(calendar.getTime());
-            getsales(archeuli_dge);
+            getsales(archeuli_dge, distr_id);
         }
 
         btn_setDate.setText(archeuli_dge);
-        tTarigi.setText("დღიური რეალიზაცია\nთარიღი "+archeuli_dge);
+        tTarigi.setText("დღიური რეალიზაცია\nთარიღი " + archeuli_dge);
+
+        final ArrayList<String> distributors = new ArrayList<>();
+        distributors.add("ყველა");
+        for (int i = 0; i < Constantebi.USERsLIST.size(); i++) {
+            distributors.add(Constantebi.USERsLIST.get(i).getName());
+        }
+        SpinnerAdapter spAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, distributors);
+        sp_distr.setAdapter(spAdapter);
+
+        sp_distr.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int sel_pos, long l) {
+                if (sel_pos > 0) {
+                    for (int i = 0; i < Constantebi.USERsLIST.size(); i++) {
+                        if (Constantebi.USERsLIST.get(i).getName().equals(distributors.get(sel_pos))) {
+                            distr_id = String.valueOf(Constantebi.USERsLIST.get(i).getId());
+                        }
+                    }
+                } else {
+                    distr_id = "0";
+                }
+                getsales(archeuli_dge, distr_id);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                distr_id = "0";
+            }
+        });
 
         btn_setDate.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.N)
@@ -105,14 +156,14 @@ public class DaySaleActivity extends AppCompatActivity {
                 datePickerDialog.show();
             }
         });
+
+
     }
 
-    private void getsales(String tarigi) {
+    private void getsales(String tarigi, String distrid) {
 
-        progressDialog = ProgressDialog.show(this,"იტვირთება!", "დაელოდეთ!");
-        RequestQueue queue = Volley.newRequestQueue(this);
 
-        JsonArrayRequest request_fasebi = new JsonArrayRequest(Constantebi.URL_GET_SALEDAY+"?tarigi="+tarigi, new Response.Listener<JSONArray>() {
+        JsonArrayRequest request_fasebi = new JsonArrayRequest(Constantebi.URL_GET_SALEDAY + "?tarigi=" + tarigi + "&distrid=" + distrid, new Response.Listener<JSONArray>() {
             @Override
             public void onResponse(JSONArray response) {
                 salesDay.clear();
@@ -138,24 +189,36 @@ public class DaySaleActivity extends AppCompatActivity {
                 }
 
                 salesAdapter = new DaySalesAdapter(getApplicationContext(), salesDay);
-                saleslistView.setAdapter(salesAdapter);
-                progressDialog.dismiss();
+                nonScrolSaleslistView.setAdapter(salesAdapter);
+
+                if (!(progressDialog == null)) {
+                    if (progressDialog.isShowing()) {
+                        progressDialog.dismiss();
+                    }
+                }
                 showAtherInfo(salesDay);
+                requestInProgres = false;
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 Toast.makeText(DaySaleActivity.this, error.getMessage(), Toast.LENGTH_LONG).show();
                 progressDialog.dismiss();
+                requestInProgres = false;
             }
         });
 
-        queue.add(request_fasebi);
+        if (!requestInProgres && requestNeeded) {
+            progressDialog = ProgressDialog.show(this, "იტვირთება!", "დაელოდეთ!");
+            queue.add(request_fasebi);
+            requestInProgres = true;
+        }
+        requestNeeded = true;
     }
 
     private void showAtherInfo(ArrayList<SaleInfo> salesDay) {
-        int k3=0, k5=0;
-        double pr=0;
+        int k3 = 0, k5 = 0;
+        double pr = 0;
         for (int i = 0; i < salesDay.size(); i++) {
             k3 += salesDay.get(i).getK30();
             k5 += salesDay.get(i).getK50();
